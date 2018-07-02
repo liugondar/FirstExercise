4b5581735da4198ad946b7c919d6efc4a31f2a7c
liugondar <liugondar@gmail.com>
2018-07-02 21:48:21 +0700

    init project

---
+++ src/Components/CellItem/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class CellItem extends Component {
+  render() {
+    return (
+      <View>
+        <Text />
+      </View>
+    );
+  }
+}
+
+export default CellItem;
